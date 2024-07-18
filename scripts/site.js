fe9d95becdf3712465a14d4d8b2cba88cb3c4f0b
Adam Katsu <adamkatsu369@gmail.com>
2024-07-18 17:33:12 +0700

    mega menu

--- scripts/site.js
+++ scripts/site.js
@@ -65,15 +65,17 @@ navDropToggle.forEach((x) => {
 const menuBtn = document.querySelectorAll('.nav-menu-btn');
 const navMain = document.querySelector('.nav-main');
 
-for(btn of menuBtn) {
-  btn.addEventListener('click', () => {
-    console.log('click ok');
-    navMain.classList.toggle('hide');
-
-    if(navMain.classList.contains('hide')) {
-      document.querySelector('body').style.overflow = 'unset';
-    } else {
-      document.querySelector('body').style.overflow = 'hidden';
-    }
-  })
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+  for(btn of menuBtn) {
+    btn.addEventListener('click', () => {
+      console.log('click ok');
+      navMain.classList.toggle('hide');
+  
+      if(navMain.classList.contains('hide')) {
+        document.querySelector('body').style.overflow = 'unset';
+      } else {
+        document.querySelector('body').style.overflow = 'hidden';
+      }
+    })
+  }
+});
